6df87b5e2180725676b3a12c792725ad0e783e6d
rkaur <rkaur@rkaur-MacBook-Pro.local>
2015-02-21 16:34:59 -0800

    Initial Commit

---
+++ isItPrime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  isItPrime
+//
+//  Created by rkaur on 2/21/15.
+//  Copyright (c) 2015 com.rkaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
