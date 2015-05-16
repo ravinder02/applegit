278b83f649a26c87ac1168095565c78d4f230388
rkaur <rkaur@rkaur-MacBook-Pro.local>
2015-05-16 11:48:54 -0700

    initial commit

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enteredText: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBAction func checkButtin(sender: AnyObject)
+    {
+        var number = enteredText.text.toInt()
+       
+        if number != nil
+        {
+            if number == 0 || number == 1 {
+            errorLabel.text = "Number is not Prime"
+            }
+            
+            else if number! == 2  {
+                 errorLabel.text = "Number is Prime"
+            }
+            else {
+            for ( var i = 2; i < number; i++){
+                if (number! % i == 0) {
+                    self.errorLabel.text = "number is not prime"
+                    break
+                                    }
+                else {
+                    self.errorLabel.text = "number is prime"
+                }
+                }
+            }
+        }
+        else {
+            self.errorLabel.text = "Enter number"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
